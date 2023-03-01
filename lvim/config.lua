1ec917456f0ba7cdb1a97ca5f4744d3b91e19ea4
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-01 15:37:37 -0800

    added bind for outline

--- lvim/config.lua
+++ lvim/config.lua
@@ -24,6 +24,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["t"] = ":NvimTreeToggle<cr>"
 lvim.keys.normal_mode["f"] = ":Telescope current_buffer_fuzzy_find<cr>"
 lvim.keys.normal_mode["<S-T>"] = ":ToggleTerm<cr>"
+lvim.keys.normal_mode["<S-O>"] = ":SymbolsOutline<cr>"
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
