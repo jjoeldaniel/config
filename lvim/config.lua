93e6865773b4c454170f63c17c05bce9d3817fc1
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-01 16:03:31 -0800

    added neoscroll and hop

--- lvim/config.lua
+++ lvim/config.lua
@@ -22,6 +22,7 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["t"] = ":NvimTreeToggle<cr>"
+lvim.keys.normal_mode["h"] = ":HopAnywhere<cr>"
 lvim.keys.normal_mode["f"] = ":Telescope current_buffer_fuzzy_find<cr>"
 lvim.keys.normal_mode["<S-T>"] = ":ToggleTerm<cr>"
 lvim.keys.normal_mode["<S-O>"] = ":SymbolsOutline<cr>"
@@ -69,11 +70,35 @@ lvim.plugins = {
        end
      },
      {
+       "karb94/neoscroll.nvim",
+       config = function()
+       require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                    '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+        performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+      })
+       end
+     },
+     {
        "simrat39/symbols-outline.nvim",
        config = function()
         require("symbols-outline").setup()
        end
-     }
+     },
+     {
+       "phaazon/hop.nvim",
+       config = function()
+        require("hop").setup { keys = 'etovxqpdygfblzhckisuran' }
+       end
+     },
 }
 
 require('telescope').load_extension('projects')
