7b92bc25c569b5da344afdfc356dfd75c4499a07
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-02 03:03:36 -0800

    renamed update -> pull

---
+++ lvim/pull.sh
@@ -0,0 +1,3 @@
+cp ~/.config/lvim/config.lua config.lua
+
+echo "Successfully pulled config!"
--- lvim/update.sh
+++
@@ -1,3 +0,0 @@
-cp ~/.config/lvim/config.lua ~/Projects/config/lvim/config.lua
-
-echo "Successfully pulled config!"
