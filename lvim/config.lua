24e6e4db8332ae5046f8089334df724480ef4d21
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-02 02:38:45 -0800

    update config

--- lvim/config.lua
+++ lvim/config.lua
@@ -22,7 +22,7 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["t"] = ":NvimTreeToggle<cr>"
-lvim.keys.normal_mode["h"] = ":HopAnywhere<cr>"
+lvim.keys.normal_mode["h"] = ":HopWord<cr>"
 lvim.keys.normal_mode["f"] = ":Telescope current_buffer_fuzzy_find<cr>"
 lvim.keys.normal_mode["<S-T>"] = ":ToggleTerm<cr>"
 lvim.keys.normal_mode["<S-O>"] = ":SymbolsOutline<cr>"
@@ -60,23 +60,36 @@ lvim.plugins = {
     "karb94/neoscroll.nvim",
     event = "WinScrolled",
     config = function()
-    require('neoscroll').setup({
-          -- All these keys will be mapped to their corresponding default scrolling animation
-          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-          '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-          hide_cursor = true,          -- Hide cursor while scrolling
-          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-          use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-          easing_function = nil,        -- Default easing function
-          pre_hook = nil,              -- Function to run before the scrolling animation starts
-          post_hook = nil,              -- Function to run after the scrolling animation ends
-          })
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,            -- Hide cursor while scrolling
+        stop_eof = true,               -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false,   -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false,     -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true,   -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,         -- Default easing function
+        pre_hook = nil,                -- Function to run before the scrolling animation starts
+        post_hook = nil,               -- Function to run after the scrolling animation ends
+      })
     end
   },
-  {'simrat39/symbols-outline.nvim'},
-  {'phaazon/hop.nvim'},
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+    end
+  },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
 }
 
 require('telescope').load_extension('projects')
