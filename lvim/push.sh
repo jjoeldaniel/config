18b46f63f06e4075be2c7156443be21d37ad6fe9
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-02 04:50:11 -0800

    switched to variables

--- lvim/push.sh
+++ lvim/push.sh
@@ -2,10 +2,13 @@
 
 git pull --quiet
 
-if diff "./config.lua" "/home/$USER/.config/lvim/config.lua" >/dev/null ; then
+repo_file="./config.lua"
+config_file="/home/$USER/.config/lvim/config.lua"
+
+if diff $repo_file $config_file >/dev/null ; then
   echo "No difference found"
 else
   echo "Updating config file"
-  cp "./config.lua" "/home/$USER/.config/lvim"
+  cp $repo_file $config_file
   echo "Configuration file updated"
 fi
