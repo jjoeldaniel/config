164e9e24c7cebcd8376d775ad277882c186e565d
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-04 23:53:45 -0800

    added quotes around file paths

--- lvim/pull.sh
+++ lvim/pull.sh
@@ -3,7 +3,7 @@
 repo_file="./config.lua"
 config_file="$HOME/.config/lvim/config.lua"
 
-if diff $repo_file $config_file >/dev/null ; then
+if diff "$repo_file" "$config_file" >/dev/null ; then
   echo "No difference found"
 else
   echo "Updating repo config file"
--- lvim/push.sh
+++ lvim/push.sh
@@ -5,7 +5,7 @@ git pull --quiet
 repo_file="./config.lua"
 config_file="$HOME/.config/lvim/config.lua"
 
-if diff $repo_file $config_file >/dev/null ; then
+if diff "$repo_file" "$config_file" >/dev/null ; then
   echo "No difference found"
 else
   echo "Updating config file"
