264693926db8ec71e6dceb3da710fff47b7b5d95
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-05 16:28:48 -0800

    scripts now work from outside directories

--- lvim/pull.sh
+++ lvim/pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-repo_file="./config.lua"
+script_dir=$(dirname "$0")
+repo_file="$script_dir/config.lua"
 config_file="$HOME/.config/lvim/config.lua"
 
 if diff "$repo_file" "$config_file" >/dev/null ; then
--- lvim/push.sh
+++ lvim/push.sh
@@ -2,7 +2,8 @@
 
 git pull --quiet
 
-repo_file="./config.lua"
+script_dir=$(dirname "$0")
+repo_file="$script_dir/config.lua"
 config_file="$HOME/.config/lvim/config.lua"
 
 if diff "$repo_file" "$config_file" >/dev/null ; then
