45bc38bf9f1d73b092f328269007b5868f86dcf4
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-06 23:35:05 -0800

    Added nvim config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+
+-- ui
+vim.api.nvim_set_keymap("n", "<S-T>", ":NvimTreeToggle<cr>", {})
+vim.api.nvim_set_keymap("n", "<S-O>", ":SymbolsOutline<cr>", {})
+vim.api.nvim_set_keymap("n", "<S-F>", ":Telescope current_buffer_fuzzy_find<cr>", {})
+vim.api.nvim_set_keymap("n", "<S-P>", ":Telescope project<cr>", {})
+
+-- hop
+vim.api.nvim_set_keymap("n", "h", ":HopChar1<cr>", {})
+vim.api.nvim_set_keymap("n", "hh", ":HopChar1<cr>", {})
+vim.api.nvim_set_keymap("n", "<S-H>", ":HopWord<cr>", {})
+
+--- minimap
+vim.api.nvim_set_keymap("n", "<S-M>", ":MinimapToggle<cr>", {})
+
+-- neoscroll
+vim.api.nvim_set_keymap("n", "w", "<cmd>lua require('neoscroll').scroll(-10, true, 250)<cr>", {})
+vim.api.nvim_set_keymap("n", "s", "<cmd>lua require('neoscroll').scroll(10, true, 250)<cr>", {})
---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-project.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("project")
+      end,
+    },
+  },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
+  { "wfxr/minimap.vim" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
+  { "mrjones2014/nvim-ts-rainbow" },
+  {
+    "nacro90/numb.nvim",
+    event = "BufRead",
+    config = function()
+      require("numb").setup({
+        show_numbers = true, -- Enable 'number' for the window while peeking
+        show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+      })
+    end,
+  },
+}
