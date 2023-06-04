e71cb707aa8ef0edc1e8ca4903c89be9393ab6c2
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-04 07:04:18 -0700

    added aliases

--- config.fish
+++ config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set PATH $PATH ~/.cargo/bin
     starship init fish | source
+
+    # aliases
+    alias cat="bat --theme OneHalfDark -p"
+    alias tree="exa -T -L"
 end
